29a8a013b79624d63abf4504604810f9762bd5bf
Sara Ekman <sara.d.ekman@gmail.com>
2018-11-13 10:56:55 +0100

    Format to pep8

--- tests/test_json_converter.py
+++ tests/test_json_converter.py
@@ -13,18 +13,18 @@ def create_test_data_element(legacy_id, type, label, concept_id, legacy_num_id):
     return od
 
 
-testData = []
-testData.append(create_test_data_element('5370',
+test_data = []
+test_data.append(create_test_data_element('5370',
                                          'jobterm',
                                          '1:e Fartygsingenjör/1:e Maskinist',
                                          '5bcde2bb-ff9b-4a8b-9a09-cf3c67b9a05e',
                                          3151))
-testData.append(create_test_data_element('5372',
+test_data.append(create_test_data_element('5372',
                                          'jobterm',
                                          '2:e Fartygsingenjör/2:e Maskinist',
                                          '5bcde2bb-9c6b-4ad0-8e1f-aa086598b663',
                                          3151))
-testData.append(create_test_data_element('5369',
+test_data.append(create_test_data_element('5369',
                                          'jobterm',
                                          '2:e Styrman',
                                          '5bcde2bb-5854-4ced-bf3e-7fccef284357',
@@ -33,4 +33,4 @@ testData.append(create_test_data_element('5369',
 
 @pytest.mark.unit
 def test_taxonomy_to_concept():
-    result = importers.new_taxonomy.json_converter.taxonomy_to_concept(testData)
+    result = importers.new_taxonomy.json_converter.taxonomy_to_concept(test_data)
