ffc9cb6ac084fbc40f60af981e48f17bae886bd2
Sara Ekman <sara.d.ekman@gmail.com>
2018-11-13 16:55:40 +0100

    Code cleanup + rename functions

--- importers/new_taxonomy/json_converter.py
+++ importers/new_taxonomy/json_converter.py
@@ -1,10 +1,19 @@
 import json
-import os
 from importers.new_taxonomy.settings import resources_folder
 from collections import defaultdict
 
 
-def concept_to_taxonomy(values):
+def save_concept_to_taxonomy_as_json(values):
+    result = map_concept_to_taxonomy(values)
+    dump_json("concept_to_taxonomy.json", result)
+
+
+def save_taxonomy_to_concept_as_json(values):
+    result = map_taxonomy_to_concept(values)
+    dump_json("taxonomy_to_concept.json", result)
+
+
+def map_concept_to_taxonomy(values):
     json_dict = {}
     for value in values:
         json_dict[value["concept_id"]] = {}
@@ -13,16 +22,8 @@ def concept_to_taxonomy(values):
         json_dict[value["concept_id"]]["label"] = value["label"]
     return json_dict
 
-def save_concept_to_taxonomy_as_json(values):
-    result = concept_to_taxonomy(values)
-    dump_json("concept_to_taxonomy.json", result)
 
-def save_taxonomy_to_concept_as_json(values):
-    result = taxonomy_to_concept(values)
-    dump_json("taxonomy_to_concept.json", result)
-
-
-def taxonomy_to_concept(values):
+def map_taxonomy_to_concept(values):
     py_dict = defaultdict(dict)
     for value in values:
         py_dict[value["type"]][value["legacy_ams_taxonomy_id"]] = {}
@@ -38,9 +39,12 @@ def dump_json(file_name, data):
         json.dump(data, fout, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 
+# todo: remove function below if it isn't used by anyone?
+"""
 def open_json(file_name):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(dir_path + "/" + file_name, "r") as fin:
         data = json.load(fin)
         for k, v in data.items():
             print(k, v)
+"""
