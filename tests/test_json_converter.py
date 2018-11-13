92c01962658896bb1ae63009ff85d97f72ad4dfb
Sara Ekman <sara.d.ekman@gmail.com>
2018-11-13 10:54:04 +0100

    format to pep8

--- tests/test_json_converter.py
+++ tests/test_json_converter.py
@@ -1,9 +1,9 @@
 from collections import OrderedDict
 import importers.new_taxonomy.json_converter
-
 import pytest
 
-def createTestDataElement(legacy_id, type, label, concept_id, legacy_num_id):
+
+def create_test_data_element(legacy_id, type, label, concept_id, legacy_num_id):
     od = OrderedDict()
     od['legacy_ams_taxonomy_id'] = legacy_id
     od['type'] = type
@@ -12,10 +12,24 @@ def createTestDataElement(legacy_id, type, label, concept_id, legacy_num_id):
     od['legacy_ams_taxonomy_num_id'] = legacy_num_id
     return od
 
+
 testData = []
-testData.append(createTestDataElement( '5370', 'jobterm',  '1:e Fartygsingenjör/1:e Maskinist', '5bcde2bb-ff9b-4a8b-9a09-cf3c67b9a05e', 3151))
-testData.append(createTestDataElement( '5372', 'jobterm',  '2:e Fartygsingenjör/2:e Maskinist', '5bcde2bb-9c6b-4ad0-8e1f-aa086598b663', 3151))
-testData.append(createTestDataElement( '5369', 'jobterm',  '2:e Styrman', '5bcde2bb-5854-4ced-bf3e-7fccef284357', 3151))
+testData.append(create_test_data_element('5370',
+                                         'jobterm',
+                                         '1:e Fartygsingenjör/1:e Maskinist',
+                                         '5bcde2bb-ff9b-4a8b-9a09-cf3c67b9a05e',
+                                         3151))
+testData.append(create_test_data_element('5372',
+                                         'jobterm',
+                                         '2:e Fartygsingenjör/2:e Maskinist',
+                                         '5bcde2bb-9c6b-4ad0-8e1f-aa086598b663',
+                                         3151))
+testData.append(create_test_data_element('5369',
+                                         'jobterm',
+                                         '2:e Styrman',
+                                         '5bcde2bb-5854-4ced-bf3e-7fccef284357',
+                                         3151))
+
 
 @pytest.mark.unit
 def test_taxonomy_to_concept():
