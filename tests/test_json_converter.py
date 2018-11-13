92a824638230afd4ba03ab57461c54e94ce22ff8
Sara Ekman <sara.d.ekman@gmail.com>
2018-11-13 10:57:51 +0100

    Format to pep8

--- tests/test_json_converter.py
+++ tests/test_json_converter.py
@@ -15,20 +15,20 @@ def create_test_data_element(legacy_id, type, label, concept_id, legacy_num_id):
 
 test_data = []
 test_data.append(create_test_data_element('5370',
-                                         'jobterm',
-                                         '1:e Fartygsingenjör/1:e Maskinist',
-                                         '5bcde2bb-ff9b-4a8b-9a09-cf3c67b9a05e',
-                                         3151))
+                                          'jobterm',
+                                          '1:e Fartygsingenjör/1:e Maskinist',
+                                          '5bcde2bb-ff9b-4a8b-9a09-cf3c67b9a05e',
+                                          3151))
 test_data.append(create_test_data_element('5372',
-                                         'jobterm',
-                                         '2:e Fartygsingenjör/2:e Maskinist',
-                                         '5bcde2bb-9c6b-4ad0-8e1f-aa086598b663',
-                                         3151))
+                                          'jobterm',
+                                          '2:e Fartygsingenjör/2:e Maskinist',
+                                          '5bcde2bb-9c6b-4ad0-8e1f-aa086598b663',
+                                          3151))
 test_data.append(create_test_data_element('5369',
-                                         'jobterm',
-                                         '2:e Styrman',
-                                         '5bcde2bb-5854-4ced-bf3e-7fccef284357',
-                                         3151))
+                                          'jobterm',
+                                          '2:e Styrman',
+                                          '5bcde2bb-5854-4ced-bf3e-7fccef284357',
+                                          3151))
 
 
 @pytest.mark.unit
