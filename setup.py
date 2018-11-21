1c0c3ec3d73bb311123f88e4cef15a5b6d912379
qeshi <qeshis@gmail.com>
2018-11-21 15:58:16 +0100

    Delete content of taxonomy folder and replace it with new_taxonomy content, and delete new_taxonomy folder.

--- setup.py
+++ setup.py
@@ -14,9 +14,8 @@ setup(
     entry_points={
         'console_scripts': [
             'import-platsannonser = importers.platsannons.main:start',
-            'import-taxonomy = importers.new_taxonomy.main:start',
+            'import-taxonomy = importers.taxonomy.main:start',
             'import-auranest = importers.auranest.main:start',
-            'import-new-taxonomy = importers.new_taxonomy.main:start'
         ],
     },
     setup_requires=["pytest-runner"],
