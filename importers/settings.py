36d60a4df16a27d2658a946ffb58271a26e8bd23
halge <oleg.lyalin@arbetsformedlingen.se>
2020-04-29 17:17:15 +0200

    remove more unused stuff

--- importers/settings.py
+++ importers/settings.py
@@ -29,15 +29,9 @@ PG_DBNAME = os.getenv('PG_DBNAME')
 PG_USER = os.getenv('PG_USER')
 PG_PASSWORD = os.getenv('PG_PASSWORD')
 PG_BATCH_SIZE = os.getenv('PG_BATCH_SIZE', 2000)
-# PG_AURANEST_TABLE = os.getenv('PG_AURANEST_TABLE', 'auranest')
 PG_PLATSANNONS_TABLE = os.getenv('PG_PLATSANNONS_TABLE', 'platsannons_la')
 PG_SSLMODE = os.getenv('PG_SSLMODE', 'require')
 
-# AURANEST_FEED_URL = os.getenv('AURANEST_FEED_URL')
-# AURANEST_DETAILS_URL = os.getenv('AURANEST_DETAILS_URL')
-# AURANEST_USER = os.getenv('AURANEST_USER')
-# AURANEST_PASSWORD = os.getenv('AURANEST_PASSWORD')
-# AURANEST_EXPIRE_PATH = 'source.removedAt'
 
 LOADER_START_DATE = os.getenv('LOADER_START_DATE', '2018-01-01')
 
@@ -446,7 +440,7 @@ platsannons_mappings = {
 }
 
 
-# For berikning (platsannonser och auranest)
+# For berikning (platsannonser)
 URL_ENRICH_TEXTDOCS_SERVICE = \
     os.getenv('URL_ENRICH_TEXTDOCS_SERVICE',
               'https://textdoc-enrichments.dev.services.jtech.se'
