7e8ec5f26309bf840fc07eb23d608ce2e8dc0b20
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-04-29 15:39:10 +0200

    removed code and tests related to auranest and postgresql cleaned up requirements files

--- tests/integration_tests/test_glitchfix_jobtechjobs.py
+++ tests/integration_tests/test_glitchfix_jobtechjobs.py
@@ -6,12 +6,7 @@ import json
 import pytest
 
 
-@pytest.mark.skip(reason="Disabled")
-@pytest.mark.integration
-def test_glitchfix():
-    print('============================', sys._getframe().f_code.co_name, '============================ ')
-    from importers.auranest.main import glitchfix
-    glitchfix()
+
 
 @pytest.mark.skip(reason="Disabled")
 @pytest.mark.integration
@@ -27,20 +22,7 @@ def test_get_glitch_jobtechjobs_ids():
     #     print(id)
 
 
-@pytest.mark.skip(reason="Disabled")
-@pytest.mark.integration
-def test_glitchfix_read_ads_from_postgres():
-    print('============================', sys._getframe().f_code.co_name, '============================ ')
 
-    # from importers.repository.elastic import get_glitch_jobtechjobs_ids
-    from importers import settings
-    from importers.repository.postgresql import read_docs_with_ids
-    # gen_ids = get_glitch_jobtechjobs_ids(max_items=100)
-    # ids = [id for id in gen_ids]
-    ids = ['3a7cb98a-4b7f-40ff-96e4-659e91716320', '3aeedba9-0db7-467e-8c4a-a18cde1394e5']
-    # pprint(ids)
-    documents = read_docs_with_ids(settings.PG_AURANEST_TABLE, ids)
-    pprint(documents)
 
 
 if __name__ == '__main__':
