812bb25b0d6d5a1237dbd68d9ce7e9d6091c1b89
baola <lina7906@gmail.com>
2020-08-26 21:13:46 +0200

    Add encoding stuff

--- importers/scrapedannons/main.py
+++ importers/scrapedannons/main.py
@@ -65,7 +65,8 @@ def bulk_fetch_ad_details(ad_batch, es_index):
 def open_the_file(es_index):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_name = 'test_data.json'
-    with open(dir_path + "/resources/" + file_name, 'r') as fil:
+    enc = 'utf-8'
+    with open(dir_path + "/resources/" + file_name, 'r', encoding=enc) as fil:
         data = json.load(fil)
         ads = bulk_fetch_ad_details(data, es_index)
     return ads
