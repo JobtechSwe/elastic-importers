c210dbad922255bf384c1ff0f31da8f0ad4bd21d
halge <oleg.lyalin@arbetsformedlingen.se>
2020-10-01 13:35:17 +0200

    NAR-445 add else logic

--- importers/settings.py
+++ importers/settings.py
@@ -35,8 +35,9 @@ LA_LAST_TIMESTAMP_MANUAL = os.getenv('LA_LAST_TIMESTAMP_MANUAL', 'false').lower(
 LA_LAST_TIMESTAMP = int(os.getenv('LA_LAST_TIMESTAMP', 0))
 # trigger to use ad format with v2 (concept_id)
 LA_ANNONS_V2 = os.getenv('LA_ANNONS_V2', 'true').lower() == 'true'
-if LA_ANNONS_V2:
-    LA_DETAILS_URL = re.sub(r"v\d", "v2", str(LA_DETAILS_URL))
+
+LA_DETAILS_URL = re.sub(r"v\d", "v2", str(LA_DETAILS_URL)) if LA_ANNONS_V2 \
+    else LA_DETAILS_URL = re.sub(r"v\d", "v1", str(LA_DETAILS_URL))
 
 # For berikning (platsannonser)
 URL_ENRICH = 'https://textdoc-enrichments.dev.services.jtech.se/enrichtextdocuments'
