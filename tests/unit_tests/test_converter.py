6cdd96b4910a5d69d798e9c40515188c564eb8ce
oskardrenske-ith <oskar.drenske@it-huset.se>
2020-10-19 15:23:59 +0200

    updated test data with legacy id unskipped date conversion test moved date conversion code to separate function to be able to test it without error logs

--- tests/unit_tests/test_converter.py
+++ tests/unit_tests/test_converter.py
@@ -9,7 +9,6 @@ log = logging.getLogger(__name__)
 @pytest.mark.unit
 @pytest.mark.parametrize("parsing_date", ['180924-00:00', '20180924T', 'mon sep 24', '00:00:00'])
 @pytest.mark.parametrize("not_parsing_date", ['20180101f', '2099-13-32', '18-09-24:01:01', '', None, []])
-@pytest.mark.skip("Exceptions pollute log")
 def test_isodate(parsing_date, not_parsing_date):
     print('==================', sys._getframe().f_code.co_name, '================== ')
     if not not_parsing_date:
