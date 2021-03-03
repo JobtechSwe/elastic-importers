7c3a58dddafcaabf042151adaf3a4a2feda23f71
halge <oleg.lyalin@arbetsformedlingen.se>
2021-03-03 03:59:18 +0100

    try#3 fix ModuleNotFoundError

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ElasticImporters',
     author='Team Narwhal',
-    version='1.0.4',
+    version='1.4.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
