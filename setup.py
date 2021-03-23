a295e717ef033218d6ff3896bad89f9305328e83
halge <oleg.lyalin@arbetsformedlingen.se>
2021-03-23 13:07:31 +0100

    add dateutil back

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version=settings.VERSION,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['valuestore', 'elasticsearch', 'zeep', 'jobtech-common'],
+    install_requires=['valuestore', 'jobtech-common', 'elasticsearch', 'zeep', 'python-dateutil'],
     package_data={'': ['**/resources/*']},
     entry_points={
         'console_scripts': [
