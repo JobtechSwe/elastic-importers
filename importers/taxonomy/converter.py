1988a027a2f293a035f2916198fe91c47891e49c
oskardrenske-ith <62339316+oskardrenske-ith@users.noreply.github.com>
2021-04-22 15:12:08 +0200

    spelling in log message

--- importers/taxonomy/converter.py
+++ importers/taxonomy/converter.py
@@ -56,11 +56,11 @@ def convert_occupation_value(value):
             convert_values.append(occupation_field)
             occupation_group['parent'] = occupation_field
         else:
-            log.error('There is not occupation field for %s' % parent_value.get('id', None))
+            log.error('There is no occupation field for %s' % parent_value.get('id', None))
         convert_values.append(occupation_group)
         occupation_name['parent'] = occupation_group
     else:
-        log.error('There is not occupation group for %s' % value.get('id', None))
+        log.error('There is no occupation group for %s' % value.get('id', None))
 
     if collection_value:  # Add collection field
         collection_value = collection_value[0]
