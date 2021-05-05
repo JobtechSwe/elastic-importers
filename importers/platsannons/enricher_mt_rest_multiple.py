c34dc776597d3c1d3983aa9af2399f9299493970
baola <lina7906@gmail.com>
2021-05-05 08:57:05 +0200

    Add occupation group to enrich

--- importers/platsannons/enricher_mt_rest_multiple.py
+++ importers/platsannons/enricher_mt_rest_multiple.py
@@ -80,7 +80,9 @@ def enrich(annonser, typeahead=True):
 def get_doc_headline_input(annons):
     sep = ' | '
     # Add occupation from structured data in headline.
-    doc_headline_occupation = ' | '.join([occupation.get('label', '') for occupation in annons.get('occupation', {})])
+    occupation = ' | '.join([occupation.get('label', '') for occupation in annons.get('occupation', {})])
+    occupation_group = ' | '.join([occupation_group.get('label', '') for occupation_group in annons.get('occupation_group', {})])
+    doc_headline_occupation = occupation + sep + occupation_group
     if not doc_headline_occupation:
         doc_headline_occupation = ''
     log.debug(f"enriched headline occupation: {doc_headline_occupation}")
